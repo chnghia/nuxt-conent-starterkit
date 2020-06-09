ec950539e6cc2f2abf00aa59bf9c6dc0350a176a
nghiach <chnghia@gmail.com>
2020-06-09 15:52:44 +0700

    update layout of content site

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="pt-16">
-    <Navbar />
+    <!-- <Navbar /> -->
     <main class="container mx-auto px-4 lg:px-8">
       <div class="flex flex-wrap relative">
         <aside
           class="h-screen w-full lg:w-1/5 fixed lg:sticky top-0 left-0 bottom-0 pt-16 lg:-mt-16 lg:block bg-white dark:bg-gray-900 lg:bg-transparent z-30 lg:border-r dark:border-gray-800"
-          :class="{ 'block': menu, 'hidden': !menu }"
+          :class="{ block: menu, hidden: !menu }"
         >
           <div class="container mx-auto overflow-auto h-full">
             <div class="lg:hidden flex-1 flex justify-center px-4 mt-8 mb-4 w-full">
-              <SearchInput />
+              <!-- <SearchInput /> -->
             </div>
             <ul class="lg:pl-0 p-4 lg:py-8 lg:pr-8">
               <li v-for="(docs, category) in categories" :key="category" class="mb-6 last:mb-0">
@@ -20,58 +20,51 @@
                       :to="toLink(doc.slug)"
                       class="px-2 rounded font-medium py-1 block text-gray-600 dark:text-gray-500 hover:text-gray-800 dark-hover:text-gray-100"
                       exact-active-class="text-green-600 bg-green-100 hover:text-green-600 dark:text-green-200 dark:bg-green-900 dark-hover:text-green-200"
-                    >{{ doc.title }}</NuxtLink>
+                      >{{ doc.title }}</NuxtLink
+                    >
                   </li>
                 </ul>
               </li>
             </ul>
             <h3 class="lg:hidden aside-title px-4">More</h3>
-            <div class="lg:hidden flex items-center px-4">
-              <a
-                href="https://twitter.com/nuxt_js"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Twitter"
-                name="Twitter"
-                class="hover:text-green-500 mr-4 ml-2"
-              >
-                <icon-twitter class="w-6 h-6" />
-              </a>
-
-              <a
-                href="https://github.com/nuxt/content"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Github"
-                name="Github"
-                class="hover:text-green-500 mr-4"
-              >
-                <icon-github class="w-6 h-6" />
-              </a>
-              <LangSwitcher class="mr-4" />
-              <ColorSwitcher />
-            </div>
+            <div class="lg:hidden flex items-center px-4"></div>
           </div>
         </aside>
         <Nuxt class="w-full lg:w-4/5" />
       </div>
     </main>
-    <TheFooter />
+    <!-- <TheFooter /> -->
   </div>
 </template>
 
 <script>
 export default {
   computed: {
+    menu: {
+      get() {
+        return this.$store.state.menu.open
+      },
+      set(val) {
+        this.$store.commit('menu/toggle', val)
+      }
+    },
     categories() {
       return this.$store.state.categories[this.$i18n.locale]
+    }
+  },
+  methods: {
+    toLink(slug) {
+      if (slug === 'index') {
+        return this.localePath('slug')
+      }
+      return this.localePath({ name: 'slug', params: { slug } })
     }
   }
 }
 </script>
 
 <style lang="postcss">
-.aside-title {
+/* .aside-title {
   @apply mb-3 text-gray-500 uppercase tracking-wide font-bold text-sm;
 }
 .dark-mode .aside-title {
@@ -81,5 +74,5 @@ export default {
   .aside-title {
     @apply mb-2 text-xs;
   }
-}
+} */
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+  theme: {
+    darkSelector: '.dark-mode',
+    extend: {
+      colors: {
+        nuxt: {
+          gray: '#243746',
+          lightgreen: '#41B38A',
+          green: '#158876'
+        }
+      },
+      fill: theme => ({
+        'nuxt-gray': theme('colors.nuxt.gray'),
+        'nuxt-lightgreen': theme('colors.nuxt.lightgreen'),
+        'nuxt-green': theme('colors.nuxt.green')
+      }),
+      stroke: theme => ({
+        'nuxt-gray': theme('colors.nuxt.gray'),
+        'nuxt-lightgreen': theme('colors.nuxt.lightgreen'),
+        'nuxt-green': theme('colors.nuxt.green')
+      })
+    }
+  },
+  variants: {
+    margin: ['responsive', 'last'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'dark', 'dark-focus', 'dark-hover'],
+    textColor: ['responsive', 'hover', 'focus', 'dark', 'dark-hover', 'dark-focus'],
+    borderColor: ['responsive', 'hover', 'focus', 'dark', 'dark-focus'],
+    borderWidth: ['responsive', 'first', 'last']
+  },
+  plugins: [require('tailwindcss-dark-mode')()]
 }
